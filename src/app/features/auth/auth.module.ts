ba137ca47ea8d1d1f706f9a5bb102c835ba39667
Rishikesh Tadka <Rishikesh.Tadka@lntinfotech.com>
2023-07-25 21:37:32 +0530

    Added shared and core modules

---
+++ src/app/features/auth/auth.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from "@angular/core";
+import { LoginComponent } from './login/login.component';
+
+@NgModule({
+    declarations: [
+  
+    LoginComponent
+  ],
+    imports: [],
+    providers: [],
+    exports:[
+    ]
+  })
+export class AuthModule{
+
+}
