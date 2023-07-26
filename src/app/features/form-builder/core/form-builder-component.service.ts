2516a634c835875ea3456978fe82b24bec6ec637
Rishikesh Tadka <Rishikesh.Tadka@lntinfotech.com>
2023-07-26 12:17:27 +0530

    Added text area

--- src/app/features/form-builder/core/form-builder-component.service.ts
+++ src/app/features/form-builder/core/form-builder-component.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from "@angular/core";
 import { ChecklistComponent } from "@shared/components/form-builder/checklist/checklist.component";
 import { DatePickerComponent } from "@shared/components/form-builder/date-picker/date-picker.component";
 import { DropdownListComponent } from "@shared/components/form-builder/dropdown-list/dropdown-list.component";
+import { TextAreaComponent } from "@shared/components/form-builder/text-area/text-area.component";
 import { TextBoxComponent } from "@shared/components/form-builder/text-box/text-box.component";
 
 @Injectable({providedIn:"root"})
@@ -14,9 +15,9 @@ export class FormBuilderComponentService{
 
     private setComponents():void{
         this.components.set("text-box",TextBoxComponent);
-        this.components.set("text-area",DatePickerComponent);
-        this.components.set("date-picker",DropdownListComponent);
-        this.components.set("ddl",ChecklistComponent);
+        this.components.set("text-area",TextAreaComponent);
+        this.components.set("date-picker",DatePickerComponent);
+        this.components.set("ddl",DropdownListComponent);
     }
 
     public getComponents():Map<string,any>{
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -4,17 +4,26 @@ import { DropdownListComponent } from './components/form-builder/dropdown-list/d
 import { TextBoxComponent } from './components/form-builder/text-box/text-box.component';
 import { ChecklistComponent } from './components/form-builder/checklist/checklist.component';
 import { FormsModule } from "@angular/forms";
-import {MatInputModule} from '@angular/material/input';
-import {MatFormFieldModule} from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { TextAreaComponent } from './components/form-builder/text-area/text-area.component';
+import {
+  DateAdapter,
+  MAT_DATE_FORMATS,
+  MatDateFormats,
+  MatNativeDateModule,
+} from '@angular/material/core';
 
 @NgModule({
     declarations: [
     DatePickerComponent,
     DropdownListComponent,
     TextBoxComponent,
-    ChecklistComponent
+    ChecklistComponent,
+    TextAreaComponent
   ],
-    imports: [FormsModule,MatFormFieldModule,MatInputModule],
+    imports: [FormsModule,MatFormFieldModule,MatInputModule,MatDatepickerModule,MatNativeDateModule],
     providers: [],
     exports:[
         DatePickerComponent,
